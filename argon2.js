a8b7e9b2411fa5769917699aed8f675c06891ec1
Jonas L <1325712+JonL1@users.noreply.github.com>
2021-05-26 21:48:56 +0200

    Add files via upload
    Fixed parsing of password in hash function.

--- argon2.js
+++ argon2.js
@@ -3,14 +3,15 @@ const argon2 = require("argon2");
 exports.hash = async function (options) {
 
     let argon2Options = {hashLength: options.hashLength || 32, timeCost: options.timeCost || 3, memoryCost: options.memoryCost || 4096, parallelism: options.parallelism || 1}
+    let password = this.parse(options.password)
     
     switch (options.method || 'argon2i') {
         case 'argon2d':
-            return await argon2.hash(options.password, { type: argon2.argon2d, ...argon2Options }) 
+            return await argon2.hash(password, { type: argon2.argon2d, ...argon2Options }) 
         case 'argon2i':
-            return await argon2.hash(options.password, { type: argon2.argon2i, ...argon2Options })     
+            return await argon2.hash(password, { type: argon2.argon2i, ...argon2Options })     
         case 'argon2id':
-            return await argon2.hash(options.password, { type: argon2.argon2id, ...argon2Options }) 
+            return await argon2.hash(password, { type: argon2.argon2id, ...argon2Options }) 
       } 
   };
 
